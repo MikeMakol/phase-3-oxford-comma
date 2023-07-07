ae85a20a1dbe6f88fcfddc6e28fd359e2770b8cd
Mike <mikemakol45@gmail.com>
2023-07-07 21:20:56 +0300

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.length == 0
+        ""
+    elsif array.length == 1
+        array[0]
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        last_element = array.pop
+        three_element_array = array.join(", ")
+        three_element_array + ", and " + last_element
+    end
 end
